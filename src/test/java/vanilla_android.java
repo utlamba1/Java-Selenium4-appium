b45bc781f4e0e8979d3deb06daa6c6a46ddcbb10
utlamba1 <132882858+utlamba1@users.noreply.github.com>
2023-07-12 22:10:13 +0530

    Update vanilla_android.java

--- src/test/java/vanilla_android.java
+++ src/test/java/vanilla_android.java
@@ -1,22 +1,22 @@
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
+import com.lambdatest.tunnel.Tunnel;
 import java.net.URL;
 import java.util.HashMap;
 
 public class vanilla_android {
-    public static String userName = System.getenv("LT_USERNAME") == null ? "Username"  //Add username here
-            : System.getenv("LT_USERNAME");
-    public static String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "Access_Key" //Add accessKey here
-            : System.getenv("LT_ACCESS_KEY");
+    public static String userName = "username";
+    public static String accessKey = "Access key";
 
     private static RemoteWebDriver driver;
 
     public static String status ="failed";
+    static Tunnel t;
 
-    public static void main(String args[]) throws MalformedURLException, InterruptedException {
+    public static void main(String args[]) throws Exception {
+        //Tunnel t;
 
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -28,6 +28,14 @@ public class vanilla_android {
             ltOptions.put("build", "Java Selenium 4");
             ltOptions.put("name", "Sample Test Java");
             capabilities.setCapability("LT:Options", ltOptions);  // LT:Options
+
+            t = new Tunnel();
+            HashMap<String, String> options = new HashMap<String, String>();
+            options.put("user", userName);
+            options.put("key", accessKey);
+    
+            //start tunnel
+            t.start(options);
 
             driver = new RemoteWebDriver(new URL("https://" +userName + ":" + accessKey + "@hub.lambdatest.com/wd/hub"), capabilities);
 
@@ -43,5 +51,7 @@ public class vanilla_android {
         }
         ((JavascriptExecutor) driver).executeScript("lambda-status="+status);
         driver.quit();
+        t.stop();
     }
     }
+
